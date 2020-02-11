6c91873b00a6cda662b434e7e9219d899e08609d
kovalenko_ng@bk.ru <kovalenko_ng@bk.ru>
2020-02-11 22:15:36 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+      name='roman_nums',
+      version='0.0.1',
+      license='MIT',
+      description='Library for working with roman numbers',
+      packages=['roman_nums'],
+      author='Kovalenko Nikolay',
+      author_email='kovalenko.n.r-g@yandex.ru',
+      zip_safe=False,
+      install_require=[],
+      classifiers=[
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8'
+      ]
+)
